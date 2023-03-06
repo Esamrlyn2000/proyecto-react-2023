37cac6577f323a0d6381384864690eb792229f47
Esmarlyn Agustin Rodriguez Hernandez <joelrh2000@hotmail.com>
2023-03-06 17:35:54 -0400

    Se agrego el diceño de las tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react"
+import axios from "axios";
+import Cripto from "./Cripto";
+import "./App.css";
+
 
 function App() {
 
+  const API_URL = import.meta.env.VITE_API_URL;
   const [criptos, setCriptos] = useState();
 
   useEffect(() => {
-    fetch("https://api.coincap.io/v2/assets")
-    .then((resp) => resp.json())
+    axios.get(`${API_URL}assets`)
     .then((data) => {
-      setCriptos(data.data)
+      setCriptos(data.data.data)
     })
     .catch(() => {
       console.error("la peticion fallo")
@@ -18,15 +22,23 @@ function App() {
   if(!criptos) return <span> Cargando...</span>
 
 return (
-    <>
-      <h1>Lista de Criptomonedas</h1>
-      <ol>
-        {criptos.map(({name,priceUsd}) => (
-          <li>Nombre: {name} Precio: {priceUsd}</li>
-        ))}
-      </ol>
+    <div className="app-container">
+      <h2>Lista de Criptomonedas</h2>
+      <div className="cripto-container">
+        {
+        criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+          <Cripto 
+          key={id} 
+          name={name} 
+          priceUSD={priceUsd} 
+          symbol={symbol} 
+          changePercent24Hr={changePercent24Hr}
+          />
+        ))
+        }
+      </div>
 
-    </>
+    </div>
     
   )
 }
